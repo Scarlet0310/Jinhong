7ae592b82d04e3100718d5bf443ef684cd1bde60
Jinhong <jh03102000@gmail.com>
2024-01-09 22:14:25 +0900

    fitst --

---
+++ src/main/java/com/example/jinhong/JinhongApplication.java
@@ -0,0 +1,13 @@
+package com.example.jinhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinhongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinhongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jinhong/JinhongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jinhong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinhongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
